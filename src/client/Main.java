0f5cdb5057a4f2b13e6ada674e1e05dc001ca58f
Floriane Paris <floriane99.paris@free.fr>
2022-10-03 14:11:22 +0200

    Client interface

--- src/client/Main.java
+++ src/client/Main.java
@@ -6,6 +6,7 @@ import interfaces.IClientBox;
 import interfaces.IConnection;
 import interfaces.IVODService;
 import exceptionHelper.SignInFailed;
+import server.Connection;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -26,56 +27,75 @@ public class Main {
         //local
         IConnection connection = (IConnection) Naming.lookup("rmi://localhost:2001/Connection");
 
-        System.out.println("Let's have a new account");
+        //System.out.println("Let's have a new account");
 
-        boolean signedIn = false;
-        String mail = null;
-        String pwd = null;
-        while (!signedIn){
-            System.out.println("Please enter your mail");
-            mail = scanner.nextLine();
-            System.out.println("Please enter your password");
-            pwd = scanner.nextLine();
-            try {
-                signedIn = connection.signIn(mail,pwd);
-            } catch (SignInFailed | IOException signInFailed) {
-                System.out.println(signInFailed.getMessage());
-            }
-        }
-
-        System.out.println("Let's try to signIn again ! ");
-        try{connection.signIn(mail, pwd);}
-        catch (Exception e){
-            System.out.println("Ooooops there's already an account for mail : '"+mail+"'");
-        }
-
-        System.out.println("Then ..... Let's try to login ! ");
-        IVODService ivodService = null;
-        do {
-            try {
-                System.out.println("Please enter your mail");
-                mail = scanner.nextLine();
-                System.out.println("Please enter your password");
-                pwd = scanner.nextLine();
-                ivodService = connection.login(mail, pwd);
-            } catch (InvalidCredentialsException e) {
-                System.out.println(e.getMessage());
-            }
-        } while (ivodService == null);
-
+        IVODService ivodService = connection(connection);
 
         IClientBox myBox = new ClientBox();
         Registry reg = LocateRegistry.createRegistry(2002);
 
         clientBox = ClientBox.getInstance(1003);
         reg.rebind("ClientBox", clientBox);
+        String choice = null;
+        while(choice != "0"){
+            System.out.println("Do you want to see the catalog (1), to rent a movie (2), or to quit (0) ?");
+            choice = scanner.nextLine();
+            if (choice.equals("1")) {
+                System.out.println("Now let's see the catalog !");
+                System.out.println(ivodService.viewCatalog());
+            } else if (choice.equals("2")) {
+                System.out.println("And now let's try to rent a movie ! Enter the ISBN of the movie :");
+                String isbn = scanner.nextLine();
+                //String isbn = "14325426235324-2132";
+                System.out.println(ivodService.playmovie(isbn, myBox));
+            }
+        }
 
-
-        System.out.println("Now let's see the catalog !");
-        System.out.println(ivodService.viewCatalog());
-        System.out.println("And now let's try to rent a movie !");
-        System.out.println(ivodService.playmovie("14325426235324-2132", myBox));
         reg.unbind("ClientBox");
+    }
+
+    private static IVODService connection(IConnection connection) {
+        System.out.println("Welcome ! Do you want to create a account (1) or to login (2) ? ");
+        while(true) {
+            String choice = scanner.nextLine();
+            if (choice.equals("1")) {
+                signin(connection);
+            } else if (choice.equals("2")) {
+                return login(connection);
+            }
+        }
+    }
+
+    private static void signin(IConnection connection){
+        boolean signedIn = false;
+        while (!signedIn){
+            System.out.println("Please enter your mail");
+            String mail = scanner.nextLine();
+            System.out.println("Please enter your password");
+            String pwd = scanner.nextLine();
+            try {
+                signedIn = connection.signIn(mail,pwd);
+            } catch (SignInFailed | IOException signInFailed) {
+                System.out.println(signInFailed.getMessage());
+            }
+        }
+        connection(connection);
+    }
+
+    private static IVODService login(IConnection connection){
+        IVODService ivodService = null;
+        do {
+            try {
+                System.out.println("Please enter your mail");
+                String mail = scanner.nextLine();
+                System.out.println("Please enter your password");
+                String pwd = scanner.nextLine();
+                ivodService = connection.login(mail, pwd);
+            } catch (InvalidCredentialsException | RemoteException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (ivodService == null);
+        return ivodService;
     }
 
 }
